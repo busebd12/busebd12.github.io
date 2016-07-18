f0a5cbede543573b3f69c98e83ba13ff6b365bac
Brendan Busey <busebd12@wfu.edu>
2016-07-17 20:40:02 -0400

    Integrated Angular JS code and added Resume section

---
+++ js/controllers/MainController.js
@@ -0,0 +1,45 @@
+app.controller('MainController', ['$scope', function($scope)
+{
+	$scope.home_button="Home";
+
+	$scope.about_me_button="About Me";
+
+	$scope.coder_button="Coder";
+
+	$scope.resume_button="Résumé";
+
+	$scope.this_site_button="This Site";
+
+	$scope.aboutMe="ABOUT ME";
+
+	$scope.greeting="Hi, my name is Brendan Busey";
+
+	$scope.subgreeting="{ Let me tell you about myself }";
+
+	$scope.coder="CODER";
+
+	$scope.c_plus_plus="{{ C++ }}";
+
+	$scope.latex="{{ LaTeX }}";
+
+	$scope.web_development="{{ Web Development }}";
+
+	$scope.resume="RÉSUMÉ";
+
+	$scope.work_with_me="Want to work with me? Let's get in touch!";
+
+	$scope.name="Brendan Busey";
+
+	$scope.location="Arlington, VA";
+
+	$scope.phone="+1 (703)-593-0189";
+
+	$scope.home="Home";
+
+	$scope.this_site="This site";
+
+	$scope.about_me="About Me";
+
+	$scope.stackoverflow="S/O";
+
+}]);
